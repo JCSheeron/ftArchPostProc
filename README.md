ab30bb83192a5f78556759fa941a5fe2d4c6c55e
JCSheeron <jcs@san.rr.com>
2017-11-17 18:14:33 -0800

    Resample functionality is complete and minimal testing has been completed.  The readme file, argument help (-h option) and descriptive comments at the start of the program were improved.
    This version also includes start and end time and value filtering.
    
    Changes to be committed:
    	modified:   README.md
    	modified:   ftArchPostProc.py

--- README.md
+++ README.md
@@ -1,6 +1,104 @@
-# ftArchPostProc
-Final Test Archive Post Processing
-Python program which takes in archive data and does stuff with it ...
+ ftArchPostProc
 
+ Final Test Archive Data Post Processing
+ This program accepts an input csv file, post processes it, and creates a csv
+ output file.
 
+ In the case of a historical trend generated file (the -t command line
+ argument), the data columns are as follows:
+ Tag1 TimeStamp, Tag1 Value, Tag2 TimeStamp, Tag2Timestamp ...
+ and the timestamps are not synchronized.
+
+ In the case of a archive export file (the -a command line argument), the data
+ columns are as follows:
+ ValueId,Timestamp (YYYY-MM-DD HH:MM:SS.mmm),value,quality,flags
+ and there are normally multiple valueIDs each at multiple timestamps.
+ Note: The -h and -a options are mutually exclusive, and one or the other must
+ be specified. 
+
+ Given an input file, the program will produce a *.csv file with the name
+ specified as the outputFileName with the format:
+ Timestamp, Tag1 Value, Tag2 Value ...
+
+ Field delimiters can be specified for the input and output files. The
+ default field delimiter is the comma (","). If another delimiter needs to
+ be specified, it can be done so using the -sd, -sourceDelimiter, -dd, or
+ -destDelimiter options. If more than one character is specified, the
+ delimiter will be interpreted as a regular expression.
+
+ File encoding can be specified for the input and output files. The default
+ encoding is "utf-16". If another encoding needs to be specified, it can be
+ done using the -se, -sourceEncoding, -de, or -destEncoding options.
+
+ It is assumed that the first row is a header. Tag names are derrived from the
+ first row cell contents.
+ 
+
+ Command line arguments are:
+ inputFileName (required, positional). The source data csv file.
+
+ outputFileName (required, positional). The .csv output file name.
+
+ -t, (required and mutually exclusive with -a).  Input file
+ is a historical trend export file.
+
+ -a (required and mutually exclusive with -h). Input file is a
+ archive export file.  
+
+ -se or --sourceEncoding (optional, default of "utf-16"). Source file encoding.
+
+ -sd or --sourceDelimiter (optional, default of ","). Destination file field
+ delimiter. Single character or regex.
+
+ -dd or --destDelimiter (optional, default of ","). Destination file field
+ delimiter. Single character or regex.
+
+ -de or --destEncoding (optional, default of "utf-16"). Destination file encoding.
+
+ -vq or --valueQuery (optional, default=None). Query string used to filter
+ the dataset. Default is empty, so nothing is filtered out. Use "val" to
+ represent the process value(s). For example, to filter out all
+ values < 0 or > 100,you want to keep everything else, so the filter string
+ would be:
+ "val >= 0 and val <= 100".
+
+ -st or --startTime (optional, default=None) Specify a start time.
+ Use the earliest data time stamp if not specified.
+
+ -et, or --endTime (optional, default=None) Specify an end time.
+ Use the latest data time stamp if not specified.
+
+ -rs or --resample (optional, default=None) Resample the data. This is usually
+ used to "downsample" data. For example, create an output file with 1 sample
+ per minute when given an input file with 1 sample per second. If a period
+ longer than the source data sample period is specified, then one value is
+ used to represent more than one row in the source file.  In this case, the
+ -stats option is used (see below) to specify what statistices are calculated
+ for the rolled up values. 
+ Options are (D)ay, (H)our, minu(T)e, (S)econd, mi(L)liseconds, and are not
+ case sensitive. You can put an integer in front of the option to further
+ specify a period. For example, "5S" would be a 5 second sample period. Note
+ that other options are supported by the environment, but unexpected sample
+ times may result.
+
+ -stats' (optional, default='m') Choose which statistics to calculate when
+ resampling. Ignored if not resampling (-rs must be specified for this option
+ to do anything).  Choices are: (V)alue, m(I)n, ma(X), (a)verage/(m)ean,
+ and (s)tandard deviation. Choices are not case sensitive. Default is 
+ average/mean.  In the case of the Value option, the first value available
+ which is on or after the timestamp is shown. The values between this and the
+ next sample point are thrown away. For the other options, the intermediate
+ values are used to calculate the statistic.
+
+ TODO: Move Timestamp Indexed data class (TsIdxData) to a module
+
+ Imports:
+
+from datetime import datetime, time
+from dateutil import parser as duparser
+
+import argparse
+
+import numpy as np
+import pandas as pd
 
--- ftArchPostProc.py
+++ ftArchPostProc.py
@@ -20,18 +20,22 @@
 # Given an input file, the program will produce a *.csv file with the name
 # specified as the outputFileName with the format:
 # Timestamp, Tag1 Value, Tag2 Value ...
-# TODO: More detail about what happens to combine and normalize timestamps
 #
-# The default field delimiter is the comma (","). If another delimiter needs to
-# be specified, it can be done so using the -d (delimiter) option. If more than
-# one character is specified, the delimiter will be interpreted as a regular
-# expression.
+# Field delimiters can be specified for the input and output files. The
+# default field delimiter is the comma (","). If another delimiter needs to
+# be specified, it can be done so using the -sd, -sourceDelimiter, -dd, or
+# -destDelimiter options. If more than one character is specified, the
+# delimiter will be interpreted as a regular expression.
 #
-# It is assumed that the first row is a header. If there is no header row, then
-# use the -noheader command line option
+# File encoding can be specified for the input and output files. The default
+# encoding is "utf-16". If another encoding needs to be specified, it can be
+# done using the -se, -sourceEncoding, -de, or -destEncoding options.
+#
+# It is assumed that the first row is a header. Tag names are derrived from the
+# first row cell contents.
 # 
 #
-# Command line paraemters are:
+# Command line arguments are:
 # inputFileName (required, positional). The source data csv file.
 #
 # outputFileName (required, positional). The .csv output file name.
@@ -42,9 +46,52 @@
 # -a (required and mutually exclusive with -h). Input file is a
 # archive export file.  
 #
-#-d or --delimiter (optional, default of "'"). Delimiter. Single character or regex.
+# -se or --sourceEncoding (optional, default of "utf-16"). Source file encoding.
 #
-# -noheader (optional) Do not treat the first row as header info.
+# -sd or --sourceDelimiter (optional, default of ","). Destination file field
+# delimiter. Single character or regex.
+
+# -dd or --destDelimiter (optional, default of ","). Destination file field
+# delimiter. Single character or regex.
+#
+# -de or --destEncoding (optional, default of "utf-16"). Destination file encoding.
+#
+# -vq or --valueQuery (optional, default=None). Query string used to filter
+# the dataset. Default is empty, so nothing is filtered out. Use "val" to
+# represent the process value(s). For example, to filter out all
+# values < 0 or > 100,you want to keep everything else, so the filter string
+# would be:
+# "val >= 0 and val <= 100".
+#
+# -st or --startTime (optional, default=None) Specify a start time.
+# Use the earliest data time stamp if not specified.
+#
+# -et, or --endTime (optional, default=None) Specify an end time.
+# Use the latest data time stamp if not specified.
+#
+# -rs or --resample (optional, default=None) Resample the data. This is usually
+# used to "downsample" data. For example, create an output file with 1 sample
+# per minute when given an input file with 1 sample per second. If a period
+# longer than the source data sample period is specified, then one value is
+# used to represent more than one row in the source file.  In this case, the
+# -stats option is used (see below) to specify what statistices are calculated
+# for the rolled up values. 
+# Options are (D)ay, (H)our, minu(T)e, (S)econd, mi(L)liseconds, and are not
+# case sensitive. You can put an integer in front of the option to further
+# specify a period. For example, "5S" would be a 5 second sample period. Note
+# that other options are supported by the environment, but unexpected sample
+# times may result.
+#
+# -stats' (optional, default='m') Choose which statistics to calculate when
+# resampling. Ignored if not resampling (-rs must be specified for this option
+# to do anything).  Choices are: (V)alue, m(I)n, ma(X), (a)verage/(m)ean,
+# and (s)tandard deviation. Choices are not case sensitive. Default is 
+# average/mean.  In the case of the Value option, the first value available
+# which is on or after the timestamp is shown. The values between this and the
+# next sample point are thrown away. For the other options, the intermediate
+# values are used to calculate the statistic.
+#
+# TODO: Move Timestamp Indexed data class (TsIdxData) to a module
 
 # imports
 
@@ -94,7 +141,7 @@ class TsIdxData(object):
             self._resample = ''
         else:
             # get the string version
-            self._resample = str(resample)
+            self._resample = str(resample.upper())
 
         # Get the stats to calc. Use the passed in string or default to ave.
         if stats is None or stats == '':
@@ -428,39 +475,95 @@ print('Process start time: ' + procStart.strftime('%m/%d/%Y %H:%M:%S') + '\n')
 # **** argument parsing
 # define the arguments
 # create an epilog string to further describe the input file
-eplStr="""This program accepts an input csv file, post processes it, and
-creates a csv output file. The format of the input file is given using the 
--t (historical trend export file) or the -a (archive export) option.
+eplStr="""Final Test Archive Data Post Processing
+ This program accepts an input csv file, post processes it, and creates a csv
+ output file.
 
-Note: -a OR -t must be specified, but they are mutually exclusive.
+ In the case of a historical trend generated file (the -t command line
+ argument), the data columns are as follows:
+ Tag1 TimeStamp, Tag1 Value, Tag2 TimeStamp, Tag2Timestamp ...
+ and the timestamps are not synchronized.
 
-The format of the input file when the -t option is used is:
-Tag1 TimeStamp, Tag1 Value, Tag2 TimeStamp, Tag2Timestamp ...
-and the timestamps are not synchronized.  There are normally multiple tags and 
-multiple timestamps.
+ In the case of a archive export file (the -a command line argument), the data
+ columns are as follows:
+ ValueId,Timestamp (YYYY-MM-DD HH:MM:SS.mmm),value,quality,flags
+ and there are normally multiple valueIDs each at multiple timestamps.
+ Note: The -h and -a options are mutually exclusive, and one or the other must
+ be specified. 
 
-The format of the input file when the -a option is used is:
-ValueId,Timestamp (YYYY-MM-DD HH:MM:SS.mmm),value,quality,flags
-and there are normally multiple valueIDs with multiple timestamps.
+ Given an input file, the program will produce a *.csv file with the name
+ specified as the outputFileName with the format:
+ Timestamp, Tag1 Value, Tag2 Value ...
 
-The default field delimiter is a comma (\",\"). If a different delimiter is
-used for the source or destination file(s), it can be specified with the
--sd or --sourcedelimiter, or -dd or -destdelimiter options.
+ Field delimiters can be specified for the input and output files. The
+ default field delimiter is the comma (","). If another delimiter needs to
+ be specified, it can be done so using the -sd, -sourceDelimiter, -dd, or
+ -destDelimiter options. If more than one character is specified, the
+ delimiter will be interpreted as a regular expression.
 
-The first row is assumed to be header data (names).
+ File encoding can be specified for the input and output files. The default
+ encoding is "utf-16". If another encoding needs to be specified, it can be
+ done using the -se, -sourceEncoding, -de, or -destEncoding options.
 
-The data can be filtered if a query string is specified.  The specified string
-is used as a query string when the data is populated. The query string is
-specified by the -vq or --valueQuery option.  "val" represents process values.
+ It is assumed that the first row is a header. Tag names are derrived from the
+ first row cell contents.
+ 
 
-As an alternative to a query string, a start and/or end timestamp can be
-specified using the -st or --starttime and -et or --endtime options.
-The data will be merged over this time period.  If they are not specified, the
-start and end times are derived from the data.
+ Command line arguments are:
+ inputFileName (required, positional). The source data csv file.
 
-File encoding for the source or destination files  can be specified with the
--se or --sourceencoding, or -de or --destencoding options.  Default
-encoding is utf_16.\n """
+ outputFileName (required, positional). The .csv output file name.
+
+ -t, (required and mutually exclusive with -a).  Input file
+ is a historical trend export file.
+
+ -a (required and mutually exclusive with -h). Input file is a
+ archive export file.  
+
+ -se or --sourceEncoding (optional, default of "utf-16"). Source file encoding.
+
+ -sd or --sourceDelimiter (optional, default of ","). Destination file field
+ delimiter. Single character or regex.
+
+ -dd or --destDelimiter (optional, default of ","). Destination file field
+ delimiter. Single character or regex.
+
+ -de or --destEncoding (optional, default of "utf-16"). Destination file encoding.
+
+ -vq or --valueQuery (optional, default=None). Query string used to filter
+ the dataset. Default is empty, so nothing is filtered out. Use "val" to
+ represent the process value(s). For example, to filter out all
+ values < 0 or > 100,you want to keep everything else, so the filter string
+ would be: "val >= 0 and val <= 100".
+
+ -st or --startTime (optional, default=None) Specify a start time.
+ Use the earliest data time stamp if not specified.
+
+ -et, or --endTime (optional, default=None) Specify an end time.
+ Use the latest data time stamp if not specified.
+
+ -rs or --resample (optional, default=None) Resample the data. This is usually
+ used to "downsample" data. For example, create an output file with 1 sample
+ per minute when given an input file with 1 sample per second. If a period
+ longer than the source data sample period is specified, then one value is
+ used to represent more than one row in the source file.  In this case, the
+ -stats option is used (see below) to specify what statistices are calculated
+ for the rolled up values. 
+ Options are (D)ay, (H)our, minu(T)e, (S)econd, mi(L)liseconds, and are not
+ case sensitive. You can put an integer in front of the option to further
+ specify a period. For example, "5S" would be a 5 second sample period. Note
+ that other options are supported by the environment, but unexpected sample
+ times may result.
+
+ -stats' (optional, default='m') Choose which statistics to calculate when
+ resampling. Ignored if not resampling (-rs must be specified for this option
+ to do anything).  Choices are: (V)alue, m(I)n, ma(X), (a)verage/(m)ean,
+ and (s)tandard deviation. Choices are not case sensitive. Default is 
+ average/mean.  In the case of the Value option, the first value available
+ which is on or after the timestamp is shown. The values between this and the
+ next sample point are thrown away. For the other options, the intermediate
+ values are used to calculate the statistic."""
+
 
 descrStr="Post Processing of historical trend or archive data files."
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, \
@@ -487,18 +590,27 @@ parser.add_argument('-st', '--startTime', default=None, metavar='', \
 parser.add_argument('-et', '--endTime', default=None, metavar='', \
                     help='Specify an end time. Use the data if not specified.')
 parser.add_argument('-rs', '--resample', default=None, metavar='', \
-                    help='Resample. If a period longer than the source data \
-sample period is specified, then one value is used to represent many values. \
-A min, max, mean and standard deviation values are generated.\
-Default is 1 second ("S"), and no min/max/avg/std dev is calculated.\
-Options are (D)ay, (H)our, minu(T)e, (S)econd, mi(L)liseconds, and are case \
-sensitive. You can put an integer in front of the option to further specify \
-a period. For example, "5S" would be a 5 second sample period.')
+                    help='Resample the data. This is usually \
+ used to "downsample" data. For example, create an output file with 1 sample \
+ per minute when given an input file with 1 sample per second. If a period \
+ longer than the source data sample period is specified, then one value is \
+ used to represent more than one row in the source file.  In this case, the \
+ -stats option is used (see below) to specify what statistices are calculated \
+ for the rolled up values. \
+ Options are (D)ay, (H)our, minu(T)e, (S)econd, mi(L)liseconds, and are not \
+ case sensitive. You can put an integer in front of the option to further \
+ specify a period. For example, "5S" would be a 5 second sample period. Note \
+ that other options are supported by the environment, but unexpected sample \
+ times may result.')
 parser.add_argument('-stats', default='m', metavar='', \
-                    help='Statistics to calculate if resampling. Ignored \
-if not resampling.  Choices are: (V)alue, m(I)n, ma(X), (a)verage/(m)ean, \
-and (s)tandard deviation. Choices are not case sensitive. Default is \
-average/mean.')
+                    help='Choose which statistics to calculate when \
+ resampling. Ignored if not resampling (-rs must be specified for this option \
+ to do anything).  Choices are: (V)alue, m(I)n, ma(X), (a)verage/(m)ean, \
+ and (s)tandard deviation. Choices are not case sensitive. Default is \
+ average/mean.  In the case of the Value option, the first value available \
+ which is on or after the timestamp is shown. The values between this and the \
+ next sample point are thrown away. For the other options, the intermediate \
+ values are used to calculate the statistic.')
 
 # add -t and -a as a required, but mutually exclusive group
 typegroup = parser.add_mutually_exclusive_group(required=True)
